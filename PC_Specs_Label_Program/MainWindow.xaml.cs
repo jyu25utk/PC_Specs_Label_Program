ea04bc1e81afeb74951d56f6a28f5f3a66b5af38
Jinxiao Yu <jyu25@vols.utk.edu>
2020-08-25 21:21:55 -0400

    Working on DYMO SDK, printer can successfully print the label!

--- PC_Specs_Label_Program/MainWindow.xaml.cs
+++ PC_Specs_Label_Program/MainWindow.xaml.cs
@@ -17,7 +17,10 @@ using System.Threading;
 using System.IO.Ports;
 using System.Windows.Threading;
 using DymoSDK;
+using Microsoft.Win32;
 using DymoSDK.Implementations;
+using System.Runtime.CompilerServices;
+using System.ComponentModel;
 
 namespace PC_Specs_Label_Program
 {
@@ -28,7 +31,144 @@ namespace PC_Specs_Label_Program
     
     public partial class MainWindow : Window
     {
+        #region Props
+        IEnumerable<DymoSDK.Interfaces.IPrinter> _printers;
+
+        public IEnumerable<DymoSDK.Interfaces.IPrinter> Printers
+        {
+            get
+            {
+                if (_printers == null)
+                    _printers = new List<DymoSDK.Interfaces.IPrinter>();
+                return _printers;
+            }
+            set
+            {
+                _printers = value;
+                NotifyPropertyChanged("Printers");
+            }
+        }
+
+        public int PrintersFound
+        {
+            get { return Printers.Count(); }
+        }
+
+        string _fileName;
+        public string FileName
+        {
+            get
+            {
+                if (string.IsNullOrEmpty(_fileName))
+                    return "No file selected";
+
+                return _fileName;
+            }
+            set
+            {
+                _fileName = value;
+                NotifyPropertyChanged("FileName");
+            }
+        }
+
+        private BitmapImage _imageSourcePreview;
+
+        public BitmapImage ImageSourcePreview
+        {
+            get { return _imageSourcePreview; }
+            set
+            {
+                _imageSourcePreview = value;
+                NotifyPropertyChanged("ImageSourcePreview");
+            }
+        }
+
+
+        List<DymoSDK.Interfaces.ILabelObject> _labelObjects;
+        public List<DymoSDK.Interfaces.ILabelObject> LabelObjects
+        {
+            get
+            {
+                if (_labelObjects == null)
+                    _labelObjects = new List<DymoSDK.Interfaces.ILabelObject>();
+                return _labelObjects;
+            }
+            set
+            {
+                _labelObjects = value;
+                NotifyPropertyChanged("LabelObjects");
+            }
+        }
+
+        private DymoSDK.Interfaces.ILabelObject _selectedLabelObject;
+        public DymoSDK.Interfaces.ILabelObject SelectedLabelObject
+        {
+            get { return _selectedLabelObject; }
+            set
+            {
+                _selectedLabelObject = value;
+                NotifyPropertyChanged("SelectedLabelObject");
+            }
+        }
+
+        private string _objectValue;
+        public string ObjectValue
+        {
+            get { return _objectValue; }
+            set
+            {
+                _objectValue = value;
+                NotifyPropertyChanged("ObjectValue");
+            }
+        }
+
+
+        DymoSDK.Interfaces.IPrinter _selectedPrinter;
+        public DymoSDK.Interfaces.IPrinter SelectedPrinter
+        {
+            get { return _selectedPrinter; }
+            set
+            {
+                _selectedPrinter = value;
+                NotifyPropertyChanged("FileName");
+            }
+        }
+
+        List<string> _twinTurboRolls;
+        public List<string> TwinTurboRolls
+        {
+            get
+            {
+                if (_twinTurboRolls == null)
+                    _twinTurboRolls = new List<string>();
+                return _twinTurboRolls;
+            }
+            set
+            {
+                _twinTurboRolls = value;
+                NotifyPropertyChanged("TwinTurboRolls");
+            }
+        }
+
+        private string _selectedRoll;
+        public string SelectedRoll
+        {
+            get { return _selectedRoll; }
+            set
+            {
+                _selectedRoll = value;
+                NotifyPropertyChanged("SelectedRoll");
+            }
+        }
+        #endregion
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void NotifyPropertyChanged([CallerMemberName] String propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
         SerialPort sp = new SerialPort();
+        DymoLabel dymoSDKLabel;
+        
         int count = 0;
         string recieved_data;
         string mem_str = "N/A";
@@ -39,7 +179,8 @@ namespace PC_Specs_Label_Program
         public MainWindow()
         {
             InitializeComponent();
-            
+            SelectedPrinter = DymoPrinter.Instance.GetPrinters().ToList()[0];
+            Console.WriteLine(SelectedPrinter.Name);
         }
 
         private void PrintLabel(object sender, RoutedEventArgs e)
@@ -50,7 +191,8 @@ namespace PC_Specs_Label_Program
         
         private void PrintHotKey(object sender, KeyEventArgs e)
         {
-            
+            int copies = 1;
+            bool barcodeOrGraphsquality = false;
             if (e.Key == System.Windows.Input.Key.P)
             {
                 count = 0;
@@ -58,6 +200,26 @@ namespace PC_Specs_Label_Program
                 if (hdd_str != "N/A") count++;
                 if (ssd_str != "N/A") count++;
                 if (special_str != "N/A") count++;
+            }
+
+            dymoSDKLabel = new DymoLabel();
+            
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "DYMO files |*.label;*.dymo|All files|*.*";
+            if (openFileDialog.ShowDialog() == true)
+            {
+                FileName = openFileDialog.FileName;
+                Console.WriteLine(FileName);
+                //C:\Users\Jinxiao Yu\Desktop\Label\3 lines.dymo
+                //Load label from file path
+                dymoSDKLabel.LoadLabelFromFilePath(FileName);
+                //Get object names list
+                LabelObjects = dymoSDKLabel.GetLabelObjects().ToList();
+                SelectedLabelObject = LabelObjects[0];
+                ObjectValue = "Test";
+                dymoSDKLabel.UpdateLabelObject(SelectedLabelObject, ObjectValue);
+                Console.WriteLine("Update Finish!");
+                DymoPrinter.Instance.PrintLabel(dymoSDKLabel, SelectedPrinter.Name, copies, barcodeGraphsQuality: barcodeOrGraphsquality);
             }
         }
 
